560bf76ca6e7f5fcfc6f9a224ef330c654fdada6
muhammed umar <muhammedumaralx@gmail.com>
2023-12-30 01:38:42 +0100

    First Api created

--- backend/dist/controllers/user-controller.js
+++ backend/dist/controllers/user-controller.js
@@ -1,4 +1,12 @@
-export const getAllUsers = () => {
-    console.log("users here");
+import User from "../db/models/User.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "Confirm", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "Error found", cause: error.message });
+    }
 };
 //# sourceMappingURL=user-controller.js.map
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,3 +1,12 @@
-export const getAllUsers = () => {
-    console.log("users here")
+import User from "../db/models/User.js"
+import { Request, Response, NextFunction} from 'express'
+
+export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({message: "Confirm", users})
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({message: "Error found", cause: error.message})
+    }
 }
